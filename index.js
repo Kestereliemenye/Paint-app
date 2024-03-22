8e274bc672e195fc8e3e37ac8dc949e2dc022b40
Bedford Michael <kestereliemenye@gmail.com>
2024-03-22 12:15:03 -0700

    made few changes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 function createBox(){
     
+    //created the input
     let box =  document.createElement('input');
+    //gave it the checkbox attribute
     box.setAttribute('type', 'checkbox');
-    box.style.marginBottom = "0px";
+    box.style.marginBottom = "0px";//styled it
     box.style.marginTop = "0px";
     box.style.width = "10px";
     box.style.height = "10px";
@@ -10,10 +12,11 @@ function createBox(){
     box.style.boxSizing = "border-box";
     const papa = document.querySelector('.body')
     papa.appendChild(box);
-    
+
+    // stored all the colors in a variable
     const colors = document.querySelectorAll(".colors")
     colors.forEach((cc)=>{
-
+// this function changes the color of the checkbox after a click
         cc.addEventListener("click", doThis)
         function  doThis() {
                  box.addEventListener('mouseover', function(){
@@ -24,7 +27,7 @@ function createBox(){
                 
          }
     })
-
+// to activate the draw button 
     const drawbtn = document.querySelector(".drawbtn")
      drawbtn.addEventListener("click", function(){
          box.addEventListener("mouseover", function() {
@@ -33,12 +36,13 @@ function createBox(){
 // function draw (val) {
 //         });
 )});
-
+/// to activate the eraser button
 const erasebtn = document.querySelector(".Erasebtn")
 erasebtn.addEventListener("click", function(){
     box.addEventListener("mouseover", function() {
     box.removeAttribute('checked', true)
 })});
+// to activate the delete button
     const deletbtn = document.querySelector(".deletebtn")
     deletbtn.addEventListener("click", function(){
         box.removeAttribute('checked', true);
@@ -49,27 +53,7 @@ erasebtn.addEventListener("click", function(){
        
         
    
-    //  colors.forEach((color)=> {
-    //     color.addEventListener("click", ()=> {
-    //         draw(color.value)
-    //         initColor = color.value
-    //         box.style.accentColor = initColor
-    //     })
-    // })   
-    
 
-    
-
-
-
-    
-
-
-// let delBtn = document.createElement('button');
-// delBtn.textContent = 'Delete';
-// function delBox(){
-    
-// }
 
 for(i = 0; i < 4000; i++){
     
